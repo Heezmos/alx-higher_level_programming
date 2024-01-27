0cb86085de93c0f86708814b4c77deba86264927
Heezmos <henrisonmoseray111@gmail.com>
2024-01-27 15:20:23 +0000

    Lists and Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,10 +4,7 @@ def print_list(my_list = []):
     length_list = len(my_list)
 
     new_range = range(length_list)
-
-    for num in new_range:
-        print(num)
-
+    
     print("{}".format(length_list))
 
 print_list()
