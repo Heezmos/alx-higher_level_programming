4f6122cde73c7481bc788b849c6506487b1be480
Heezmos <henrisonmoseray111@gmail.com>
2024-01-27 15:22:31 +0000

    Lists and Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def print_list(my_list = []):
-
-    length_list = len(my_list)
-
-    new_range = range(length_list)
-    
-    print("{}".format(length_list))
-
-print_list()
+# function that prints all integers of a list.
+def print_list_integer(my_list=[]):
+    for num in range(len(my_list)):
+        print("{:d}".format(my_list[num]))
