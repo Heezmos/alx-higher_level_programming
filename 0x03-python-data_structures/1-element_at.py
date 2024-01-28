50100da1eef412bd115852fab86626c112722fc7
Heezmos <henrisonmoseray111@gmail.com>
2024-01-28 07:47:42 +0000

    Dictionaries

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Accessing elemets in a list
+Accessing elements in a list
 """
 def element_at(my_list, idx):
-	if idx < 0 or idx >= (len(my_list)-1):
-		return None
-	return (my_list[idx])
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
