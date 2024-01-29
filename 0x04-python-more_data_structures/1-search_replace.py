bc300840fcdee2c5ff700f521b2cf62355b1a6e4
Heezmos <henrisonmoseray111@gmail.com>
2024-01-29 08:27:20 +0000

    More Dictionaries

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,8 @@
 """
 function that replaces all occurrences of an element by another in a new list.
 """
+
+
 def search_replace(my_list, search, replace):
     def find_search(element):
         return element if element != search else replace
