e189170944875661f9d0c61450a35f6975913206
Heezmos <henrisonmoseray111@gmail.com>
2024-01-29 08:54:48 +0000

    More Dictionaries

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function that returns the weighted average of
+all integers tuple (<score>, <weight>)
+"""
+
+
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        num = 0
+        denom = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            denom += (tup[1])
+        return (num/denom)
+    return 0
