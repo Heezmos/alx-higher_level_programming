7759b3078735e1a39b926e88eb232dba97c2e763
Heezmos <henrisonmoseray111@gmail.com>
2024-01-29 08:46:01 +0000

    More Dictionaries

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function def roman_to_int(roman_string): that converts a Roman numeral to an integer.
+"""
+
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
+    return total
