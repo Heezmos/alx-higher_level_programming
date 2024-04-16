802d5efc01e9f67c1ac99b7211f085bf5dd8b177
Heezmos <henrisonmoseray111@gmail.com>
2024-04-16 09:46:49 -0700

    Inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+""" square class """
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """ square """
+    def __init__(self, size):
+        """ initialize """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """ str defined by this function """
+        string = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return string
